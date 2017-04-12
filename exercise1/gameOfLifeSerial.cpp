966a5d366ffd20b642ed0620bc65efe8c9b75e7b
Konstantinos Iliakis <iliakiskostis@gmail.com>
2017-04-12 16:37:05 +0300

    serial version is working

--- exercise1/gameOfLifeSerial.cpp
+++ exercise1/gameOfLifeSerial.cpp
@@ -5,9 +5,13 @@
 
 using namespace std;
 
-int next_state(const map_t & map_now, int i, int j, int N);
+int next_state(const map_t &map, int i, int j, int N);
+int count_neighb(const map_t &map, int i, int j);
+void fill_bounds(const map_t &map, int N);
 
-int main(int argc, char * argv[]){
+
+int main(int argc, char * argv[])
+{
     int N = 10;
     int Turns = 100;
     float P = 0.5;
@@ -23,32 +27,74 @@ int main(int argc, char * argv[]){
     if(argc > 2){
         P = stof(argv[3]);
     }
-    
 
     cout << "Dimension: " << N << "\n";
     cout << "Turns: " << Turns << "\n";
     cout << "Possibility: " << P << "\n";
     
-
-    auto map_now = generate_map(N, P);
-    print_map(map_now);
+    map_t map_now = (map_t) allocate2D(N+2);
+    map_t map_next = (map_t) allocate2D(N+2);
     
-    auto map_next = map_now;
-
-    for(int turn = 0; turn < Turns; turn++){
+    populate_map(map_now, N, P);
+    print_map(map_now, N);
+    fill_bounds(map_now, N);
+    
+    copy2D(map_now, map_next, N+2);
+     
+    int changes = 1; 
+    for (int turn = 0; turn < Turns && changes > 0; turn++){
         for(int i = 0; i < N; i++){
-            for(int j = 0; j< N; j++){
+            for(int j = 0; j < N; j++){
                 map_next[i][j] = next_state(map_now, i, j, N);       
             }
         }
+        fill_bounds(map_next, N);
+        changes = diff(map_now, map_next, N+2);
         swap(map_now, map_next);
     }
+     
+    print_map(map_now, N);
     
-    print_map(map_now);
-
+    cout << "Alive cells: " << cum_sum(map_now, N+2) << '\n';
+    
+    
+    deallocate2D(map_now, N+2);
+    deallocate2D(map_next, N+2);
     return 0;
 }
 
-int next_state(const map_t & map_now, int i, int j, int N){
-    return !map_now[i][j];
+
+int next_state(const map_t & map, int i, int j, int N)
+{
+    // if not boundary
+    if(i > 0 && i < N+1 && j > 0 && j < N+1){
+        int neighb = count_neighb(map, i, j);
+        if(neighb == 3) return 1;
+        else if(neighb == 2) return map[i][j];
+        else return 0;
+    } else { // boundary
+        return map[i][j];
+    }
+
 }
+
+
+int count_neighb(const map_t &map, int i, int j)
+{
+    return map[i-1][j] + map[i][j-1] + map[i+1][j] + map[i][j+1];
+}
+
+
+void fill_bounds(const map_t &map, int N){
+    map[0][0] = map[0][N+1] = 0;
+    map[N+1][0] = map[N+1][N+1] = 0;
+
+    for(int i = 0; i < N; i++){
+        map[0][i+1] = map[N][i+1];
+        map[N+1][i+1] = map[1][i+1];
+        map[i+1][0] = map[i+1][N];
+        map[i+1][N+1] = map[i+1][1];
+    }
+    
+}
+
--- exercise1/utilities.h
+++ exercise1/utilities.h
@@ -2,38 +2,77 @@
 #include <iostream>
 #include <random>
 
-typedef std::vector<std::vector<int> > map_t;
+typedef int** map_t;
 
-void print_map(const map_t &map)
+
+map_t allocate2D(int N){
+    map_t map = new int*[N];
+    for(int i = 0; i < N; i++)
+        map[i] = new int[N];
+    return map;
+}
+
+void deallocate2D(map_t &map, int N){
+    for(int i = 0; i < N; i++)
+        delete[] map[i];
+    delete[] map;
+}
+
+
+void copy2D(map_t source, map_t dest, int N){
+    for(int i = 0; i < N; i++)
+        for(int j = 0; j< N; j++)
+            dest[i][j] = source[i][j];
+
+}
+
+
+void print_map(const map_t map, int N)
 {
     std::cout << "\n";
-    for(int i = 0; i < map.size()+2; i++)
+    for(int i = 0; i < N+2; i++)
         std::cout << "# ";
     std::cout << '\n';
 
-    for(const auto &row: map){
+    for(int i = 1; i <= N; i++){
         std::cout << "# ";
-        for(const auto &c: row){
-            std::cout << c << " ";
+        for(int j = 1; j<= N; j++){
+            std::cout << map[i][j] << " ";
         }
         std::cout << "#\n";
     }
     
-    for(int i = 0; i < map.size()+2; i++)
+    for(int i = 0; i < N+2; i++)
         std::cout << "# ";
     std::cout << "\n";
 
 }
 
 
-map_t generate_map(int N, float P)
+void populate_map(map_t &map, int N, float P)
 {
-    map_t map(N, std::vector<int>(N));
     std::default_random_engine gen;
     std::uniform_real_distribution<double> dist(0.0, 1.0);
     
-    for(auto &row: map)
-        for(auto &c: row)
-            c = dist(gen) > P;
-   return map; 
+    for(int i = 1; i <= N; i++)
+        for(int j=1; j<=N; j++)
+            map[i][j] = dist(gen) > P; 
+}
+
+int cum_sum(const map_t map, int N)
+{
+    int sum = 0;
+    for(int i = 0; i < N; i++)
+        sum += std::accumulate(&map[i][0], &map[i][N], 0);
+    return sum;
+}
+
+int diff(map_t map_now, map_t map_next, int N)
+{
+    int res = 0;
+    for(int i = 0; i < N; i++)
+        for(int j = 0; j < N; j++)
+            res += (map_now[i][j] - map_next[i][j]) != 0;
+    return res;
+    
 }
