a646cd719639fcb3cf8554d7c635ef20cd28ac53
Konstantinos Iliakis <iliakiskostis@gmail.com>
2017-04-13 17:34:59 +0300

    finished serial version of parallelSearch

--- parallelSearch/parallelSearchSerial.cpp
+++ parallelSearch/parallelSearchSerial.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
     char *inFileName = NULL;
 
@@ -17,18 +17,32 @@ int main(int argc, char * argv[])
     vector<int> inDataVector;
     
     FILE* inFile = fopen(inFileName, "r");
-    int number;
+    int target, number;
 
-    // while(feof(inFile) == 1){
+    fscanf(inFile, "%d", &target);
+
     while (fscanf(inFile, "%d", &number) == 1){  
         inDataVector.push_back(number);
-        printf("%d\n", number);
+        // printf("%d\n", number);
     }
-    // }
 
     int *inData = inDataVector.data();
     int inSize = inDataVector.size();
-    printf("Input Size %d\n", inSize);    
+    printf("Target element %d\n", target);
+    printf("Input Size %d\n", inSize);
+     
+    const char *outFileName = "output.data";
+    FILE *outFile = fopen(outFileName, "w");
+    
+    int occurrences = 0;
+    for(int i = 0; i < inSize; i++){
+        if(inData[i] == target){
+            fprintf(outFile, "%d\n", i);
+            occurrences++;
+        }
+    }
+    
+    printf("The target was found %d times\n", occurrences);
     return 0;
 }
 
